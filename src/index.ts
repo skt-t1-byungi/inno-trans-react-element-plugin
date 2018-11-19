8538416ccbd13980f98a361f33295ab0021cea9c
skt-t1-byungi <tiniwz@gmail.com>
2018-11-20 06:37:00 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,20 @@ import warning from 'warning'
 
 type AnyReactElement = ReactElement<any>
 
-declare module 'inno-trans/lib/types' {
-    interface ITranslator {
-        rt (key: string, values: ValueMap, opts: TransOptions): AnyReactElement
-        rtc (key: string, num: number, values: ValueMap, opts: TransOptions): AnyReactElement
-    }
+interface ITranslatorWithReact extends ITranslator {
+    rt (key: string, values?: ValueMap, opts?: TransOptions): AnyReactElement
+    rtc (key: string, num: number, values?: ValueMap, opts?: TransOptions): AnyReactElement
 }
 
 const ZERO_WIDTH_CHAR = '\u0200c'
 let REACT_ELEMENTS: AnyReactElement[] = []
 
 export = (t: ITranslator) => {
-    t.addFilter('*', collectReactElement)
-    t.rt = reactTrans
-    t.rtc = reactTranceChoice
+    const tr = t as ITranslatorWithReact
+    tr.addFilter('*', collectReactElement)
+    tr.rt = reactTrans
+    tr.rtc = reactTranceChoice
+    return tr
 }
 
 function collectReactElement (value: any) {
@@ -26,7 +26,7 @@ function collectReactElement (value: any) {
     return ZERO_WIDTH_CHAR
 }
 
-function reactTrans (this: ITranslator, key: string, values: ValueMap, opts: TransOptions) {
+function reactTrans (this: ITranslator, key: string, values?: ValueMap, opts?: TransOptions) {
     try {
         return attachReactElements(this.trans(key, values, opts))
     } finally {
@@ -34,7 +34,7 @@ function reactTrans (this: ITranslator, key: string, values: ValueMap, opts: Tra
     }
 }
 
-function reactTranceChoice (this: ITranslator, key: string,num: number, values: ValueMap, opts: TransOptions) {
+function reactTranceChoice (this: ITranslator, key: string,num: number, values?: ValueMap, opts?: TransOptions) {
     try {
         return attachReactElements(this.transChoice(key, num, values, opts))
     } finally {
@@ -47,17 +47,17 @@ function attachReactElements (message: string) {
 
     warning(
         strings.length > REACT_ELEMENTS.length + 1,
-        `[inno-trans-react-element-plugin]
-        This plugin uses "ZWNJ(\\u0200c)".
-        If the message contains "ZWNJ", it will not work.`
+        '[inno-trans-react-element-plugin]\n' +
+        'This plugin uses "ZWNJ(\\u0200c)".\n' +
+        'If the message contains "ZWNJ", it will not work.\n'
     )
 
     const children: Array<string | AnyReactElement> = []
 
     for (const str of strings) {
         children.push(str)
-        const reactElem = REACT_ELEMENTS.shift()
-        if (reactElem) children.push(reactElem)
+        const reactEl = REACT_ELEMENTS.shift()
+        if (reactEl) children.push(reactEl)
     }
 
     return createElement(Fragment, undefined, ...children)
